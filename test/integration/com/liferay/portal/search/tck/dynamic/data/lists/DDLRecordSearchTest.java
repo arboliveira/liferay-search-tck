e0f22df767fcfdf543d498a8ec630fe267270a42
André de Oliveira <andre.oliveira@liferay.com>
2015-04-07 17:39:25 -0300

    DDLRecordSearchTest moved to its own OSGi module

---
+++ test/integration/com/liferay/portal/search/tck/dynamic/data/lists/DDLRecordSearchTest.java
@@ -0,0 +1,10 @@
+package com.liferay.portal.search.tck.dynamic.data.lists;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class DDLRecordSearchTest 
+	extends com.liferay.dynamic.data.lists.search.DDLRecordSearchTest {
+
+}
