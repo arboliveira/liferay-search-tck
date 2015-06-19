3ebeb2d89073147563cb7b751559fd1790bfbd9f
André de Oliveira <andre.oliveira@liferay.com>
2015-06-19 15:23:10 -0300

    DDLRecordSearchTest can only run as OSGi now :-(

--- test/integration/com/liferay/portal/search/tck/dynamic/data/lists/DDLRecordSearchTest.java
+++ test/integration/com/liferay/portal/search/tck/dynamic/data/lists/DDLRecordSearchTest.java
@@ -1,26 +1,14 @@
 package com.liferay.portal.search.tck.dynamic.data.lists;
 
-import org.junit.ClassRule;
-import org.junit.Rule;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
+import org.arquillian.liferay.deploymentscenario.annotations.BndFile;
 
-import com.liferay.portal.kernel.test.rule.AggregateTestRule;
-import com.liferay.portal.kernel.test.rule.Sync;
-import com.liferay.portal.kernel.test.rule.SynchronousDestinationTestRule;
-import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
-import com.liferay.portal.test.rule.MainServletTestRule;
+import com.liferay.portal.search.tck.bnd.BndConstants;
 
-@RunWith(JUnit4.class)
-@Sync
+@BndFile(
+		BndConstants.BASE_DIR 
+		+ "/com/liferay/portal/search/tck/dynamic/data/lists"
+		+ "/bnd.bnd")
 public class DDLRecordSearchTest 
 	extends com.liferay.dynamic.data.lists.search.test.DDLRecordSearchTest {
-
-	@ClassRule
-	@Rule
-	public static final AggregateTestRule aggregateTestRule =
-		new AggregateTestRule(
-			new LiferayIntegrationTestRule(), MainServletTestRule.INSTANCE,
-			SynchronousDestinationTestRule.INSTANCE);
 	
 }
