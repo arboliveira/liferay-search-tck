384277df710399f11e587fdeae4a2b4dfab18d33
André de Oliveira <andre.oliveira@liferay.com>
2015-09-16 11:14:49 -0300

    I can has Gradle

--- sh/arquillian-tests-prepare.sh
+++ sh/arquillian-tests-prepare.sh
@@ -11,11 +11,18 @@ function ant_deploy()
 	ant deploy
 }
 
-function ant_test_prepare()
+function gradle_deploy()
 {
 	subdir=$1
 	cd ${LIFERAY_PORTAL_DIR}/$subdir
-	ant test-integration -Djunit.jvm=BOGUS || { 
+	${LIFERAY_PORTAL_DIR}/gradlew deploy
+}
+
+function test_prepare()
+{
+	subdir=$1
+	cd ${LIFERAY_PORTAL_DIR}/$subdir
+	${LIFERAY_PORTAL_DIR}/gradlew testIntegration -Djunit.jvm=BOGUS || { 
 		RETURN_CODE=$?
 		echo ${RETURN_CODE}
 		echo "*** IGNORING ANT FAILURE & MOVING ON! :-)"
@@ -26,10 +33,10 @@ function ant_test_prepare()
 ant_deploy portal-test
 ant_deploy portal-test-internal
 
-ant_deploy modules/apps/dynamic-data-mapping/dynamic-data-mapping-test-util
+gradle_deploy modules/apps/dynamic-data-mapping/dynamic-data-mapping-test-util
 
-ant_test_prepare modules/apps/bookmarks/bookmarks-test
-ant_test_prepare modules/apps/document-library/document-library-test
-ant_test_prepare modules/apps/dynamic-data-lists/dynamic-data-lists-test
-ant_test_prepare modules/apps/journal/journal-test
-ant_test_prepare modules/apps/wiki/wiki-test
+test_prepare modules/apps/bookmarks/bookmarks-test
+test_prepare modules/apps/document-library/document-library-test
+test_prepare modules/apps/dynamic-data-lists/dynamic-data-lists-test
+test_prepare modules/apps/journal/journal-test
+test_prepare modules/apps/wiki/wiki-test
