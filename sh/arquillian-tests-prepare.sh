975cf42fa0ff2d6a7e8634fd0fa33f1a89ca55f1
André de Oliveira <andre.oliveira@liferay.com>
2016-04-06 12:04:56 -0700

    New locations

--- sh/arquillian-tests-prepare.sh
+++ sh/arquillian-tests-prepare.sh
@@ -33,13 +33,13 @@ function test_prepare()
 
 ant_deploy portal-kernel
 ant_deploy portal-test
-ant_deploy portal-test-internal
+ant_deploy portal-test-integration
 
-gradle_deploy modules/apps/dynamic-data-mapping/dynamic-data-mapping-test-util
+gradle_deploy modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-test-util
 
-test_prepare modules/apps/bookmarks/bookmarks-test com.liferay.bookmarks.search.test.BookmarksEntrySearchTest.testSearchBaseModel
-test_prepare modules/apps/calendar/calendar-test com.liferay.calendar.search.test.CalendarSearcherTest.testBasicSearchWithOneTerm
-test_prepare modules/apps/document-library/document-library-test com.liferay.document.library.search.test.DLFileEntrySearchTest.testOrderByDDMTextField
-test_prepare modules/apps/dynamic-data-lists/dynamic-data-lists-test com.liferay.dynamic.data.lists.search.test.DDLRecordSearchTest.testBasicSearchWithJustOneTerm
-test_prepare modules/apps/journal/journal-test com.liferay.journal.search.test.JournalArticleSearchTest.testOrderByDDMTextField
-test_prepare modules/apps/wiki/wiki-test com.liferay.wiki.search.test.WikiPageSearchTest.testSpecificFields
+test_prepare modules/apps/collaboration/bookmarks/bookmarks-test com.liferay.bookmarks.search.test.BookmarksEntrySearchTest.testSearchBaseModel
+test_prepare modules/apps/collaboration/document-library/document-library-test com.liferay.document.library.search.test.DLFileEntrySearchTest.testOrderByDDMTextField
+test_prepare modules/apps/collaboration/wiki/wiki-test com.liferay.wiki.search.test.WikiPageSearchTest.testSpecificFields
+test_prepare modules/apps/forms-and-workflow/calendar/calendar-test com.liferay.calendar.search.test.CalendarSearcherTest.testBasicSearchWithOneTerm
+test_prepare modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-test com.liferay.dynamic.data.lists.search.test.DDLRecordSearchTest.testBasicSearchWithJustOneTerm
+test_prepare modules/apps/web-experience/journal/journal-test com.liferay.journal.search.test.JournalArticleSearchTest.testOrderByDDMTextField
