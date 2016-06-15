2d68de3095140a95c16309a9423a5adcb15e3023
rodrigopaulino <rodrigo.paulino@liferay.com>
2016-06-15 14:35:33 -0300

    Fix for the arquillian-test-prepare script

--- sh/arquillian-tests-prepare.sh
+++ sh/arquillian-tests-prepare.sh
@@ -46,16 +46,16 @@ ant_deploy portal-test-integration
 
 gradle_deploy modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-test-util
 
-#test_prepare modules/apps/foundation/portal-search/portal-search-test com.liferay.portal.search.facet.faceted.searcher.test.FacetedSearcherTest
-#test_prepare modules/apps/collaboration/bookmarks/bookmarks-test com.liferay.bookmarks.search.test.BookmarksEntrySearchTest 
-#test_prepare modules/apps/collaboration/bookmarks/bookmarks-test com.liferay.bookmarks.search.test.BookmarksFolderSearchTest
-#test_prepare modules/apps/collaboration/document-library/document-library-test com.liferay.document.library.search.test.DLFileEntrySearchTest
-#test_prepare modules/apps/collaboration/wiki/wiki-test com.liferay.wiki.search.test.WikiPageSearchTest
-#test_prepare modules/apps/forms-and-workflow/calendar/calendar-test com.liferay.calendar.search.test.CalendarSearcherTest
-#test_prepare modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-test com.liferay.dynamic.data.lists.search.test.DDLRecordSearchTest
-test_prepare modules/apps/web-experience/journal/journal-test com.liferay.journal.search.test.JournalArticleAssetSearchTest
-#test_prepare modules/apps/web-experience/journal/journal-test com.liferay.journal.search.test.JournalArticleIndexableTest
-test_prepare modules/apps/web-experience/journal/journal-test com.liferay.journal.search.test.JournalArticleIndexVersionsTest
-#test_prepare modules/apps/web-experience/journal/journal-test com.liferay.journal.search.test.JournalArticleSearchTest
-#test_prepare modules/apps/web-experience/journal/journal-test com.liferay.journal.search.test.JournalFolderSearchTest
-#test_prepare modules/apps/web-experience/journal/journal-test com.liferay.journal.search.test.JournalIndexerTest
+test_prepare modules/apps/foundation/portal-search/portal-search-test com.liferay.portal.search.facet.faceted.searcher.test.FacetedSearcherTest
+test_prepare modules/apps/collaboration/bookmarks/bookmarks-test com.liferay.bookmarks.search.test.BookmarksEntrySearchTest 
+test_prepare modules/apps/collaboration/bookmarks/bookmarks-test com.liferay.bookmarks.search.test.BookmarksFolderSearchTest
+test_prepare modules/apps/collaboration/document-library/document-library-test com.liferay.document.library.search.test.DLFileEntrySearchTest
+test_prepare modules/apps/collaboration/wiki/wiki-test com.liferay.wiki.search.test.WikiPageSearchTest
+test_prepare modules/apps/forms-and-workflow/calendar/calendar-test com.liferay.calendar.search.test.CalendarSearcherTest
+test_prepare modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-test com.liferay.dynamic.data.lists.search.test.DDLRecordSearchTest
+test_prepare modules/apps/web-experience/journal/journal-test com.liferay.journal.asset.test.JournalArticleAssetSearchTest
+test_prepare modules/apps/web-experience/journal/journal-test com.liferay.journal.search.test.JournalArticleIndexableTest
+test_prepare modules/apps/web-experience/journal/journal-test com.liferay.journal.service.test.JournalArticleIndexVersionsTest
+test_prepare modules/apps/web-experience/journal/journal-test com.liferay.journal.search.test.JournalArticleSearchTest
+test_prepare modules/apps/web-experience/journal/journal-test com.liferay.journal.search.test.JournalFolderSearchTest
+test_prepare modules/apps/web-experience/journal/journal-test com.liferay.journal.search.test.JournalIndexerTest
