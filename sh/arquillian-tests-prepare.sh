3fcc6fee96a1202036813f0622d82f4cf003a821
rodrigopaulino <rodrigo.paulino@liferay.com>
2016-06-15 12:40:10 -0300

    Updating the arquillian-tests-prepare script

--- sh/arquillian-tests-prepare.sh
+++ sh/arquillian-tests-prepare.sh
@@ -8,7 +8,7 @@ function ant_deploy()
 {
 	subdir=$1
 
-	figlet -f mini ant deploy $1 
+	figlet -f mini ant deploy $1 || true
 
 	cd ${LIFERAY_PORTAL_DIR}/$subdir
 	ant deploy install-portal-snapshot
@@ -18,7 +18,7 @@ function gradle_deploy()
 {
 	subdir=$1
 
-	figlet -f mini gradle deploy $1 
+	figlet -f mini gradle deploy $1 || true
 
 	cd ${LIFERAY_PORTAL_DIR}/$subdir
 	${LIFERAY_PORTAL_DIR}/gradlew deploy
@@ -29,10 +29,10 @@ function test_prepare()
 	subdir=$1
 	tests=$2
 
-	figlet -f mini test prepare $1 
+	figlet -f mini test prepare $1 || true
 
 	cd ${LIFERAY_PORTAL_DIR}/$subdir
-	${LIFERAY_PORTAL_DIR}/gradlew testIntegration --tests $tests || { 
+	${LIFERAY_PORTAL_DIR}/gradlew testIntegration --tests $tests --stacktrace || { 
 		RETURN_CODE=$?
 		echo ${RETURN_CODE}
 		echo "*** IGNORING BOGUS FAILURE & MOVING ON! :-)"
@@ -46,11 +46,16 @@ ant_deploy portal-test-integration
 
 gradle_deploy modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-test-util
 
-test_prepare modules/apps/foundation/portal-search/portal-search-test com.liferay.portal.search.facet.faceted.searcher.test.FacetedSearcherTest.testSearchByKeywords
-
-test_prepare modules/apps/collaboration/bookmarks/bookmarks-test com.liferay.bookmarks.search.test.BookmarksEntrySearchTest.testSearchBaseModel
-test_prepare modules/apps/collaboration/document-library/document-library-test com.liferay.document.library.search.test.DLFileEntrySearchTest.testOrderByDDMTextField
-test_prepare modules/apps/collaboration/wiki/wiki-test com.liferay.wiki.search.test.WikiPageSearchTest.testSpecificFields
-test_prepare modules/apps/forms-and-workflow/calendar/calendar-test com.liferay.calendar.search.test.CalendarSearcherTest.testBasicSearchWithOneTerm
-test_prepare modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-test com.liferay.dynamic.data.lists.search.test.DDLRecordSearchTest.testBasicSearchWithJustOneTerm
-test_prepare modules/apps/web-experience/journal/journal-test com.liferay.journal.search.test.JournalArticleSearchTest.testOrderByDDMTextField
+#test_prepare modules/apps/foundation/portal-search/portal-search-test com.liferay.portal.search.facet.faceted.searcher.test.FacetedSearcherTest
+#test_prepare modules/apps/collaboration/bookmarks/bookmarks-test com.liferay.bookmarks.search.test.BookmarksEntrySearchTest 
+#test_prepare modules/apps/collaboration/bookmarks/bookmarks-test com.liferay.bookmarks.search.test.BookmarksFolderSearchTest
+#test_prepare modules/apps/collaboration/document-library/document-library-test com.liferay.document.library.search.test.DLFileEntrySearchTest
+#test_prepare modules/apps/collaboration/wiki/wiki-test com.liferay.wiki.search.test.WikiPageSearchTest
+#test_prepare modules/apps/forms-and-workflow/calendar/calendar-test com.liferay.calendar.search.test.CalendarSearcherTest
+#test_prepare modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-test com.liferay.dynamic.data.lists.search.test.DDLRecordSearchTest
+test_prepare modules/apps/web-experience/journal/journal-test com.liferay.journal.search.test.JournalArticleAssetSearchTest
+#test_prepare modules/apps/web-experience/journal/journal-test com.liferay.journal.search.test.JournalArticleIndexableTest
+test_prepare modules/apps/web-experience/journal/journal-test com.liferay.journal.search.test.JournalArticleIndexVersionsTest
+#test_prepare modules/apps/web-experience/journal/journal-test com.liferay.journal.search.test.JournalArticleSearchTest
+#test_prepare modules/apps/web-experience/journal/journal-test com.liferay.journal.search.test.JournalFolderSearchTest
+#test_prepare modules/apps/web-experience/journal/journal-test com.liferay.journal.search.test.JournalIndexerTest
