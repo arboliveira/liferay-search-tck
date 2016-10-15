3aef2c21570d534919f50fe2841c21bb8fdea4b5
André de Oliveira <andre.oliveira@liferay.com>
2016-10-14 18:05:26 -0700

    Restructured script + added missing tests + html test results

--- sh/arquillian-tests-prepare.sh
+++ sh/arquillian-tests-prepare.sh
@@ -2,7 +2,70 @@
 
 set -o errexit ; set -o nounset
 
+RUN_ALL_TESTS=false
+REDEPLOY_ALL_DEPENDENCIES=false
 
+
+function main() 
+{
+
+if [ "$REDEPLOY_ALL_DEPENDENCIES" = true ] 
+then
+	redeploy_all_dependencies
+fi
+
+if [ "$RUN_ALL_TESTS" = true ]
+then
+	run_all_tests
+else
+	run_some_tests
+fi
+
+}
+
+function redeploy_all_dependencies() 
+{
+ant_deploy portal-kernel
+ant_deploy portal-test
+ant_deploy portal-test-integration
+gradle_deploy modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-test-util
+}
+
+#
+# SEE: com.liferay.portal.search.tck.AllSearchTestsArquillian
+#
+function run_all_tests()
+{
+test_run modules/apps/foundation/portal-search/portal-search-test com.liferay.portal.search.facet.faceted.searcher.test.AssetTagNamesFacetTest
+test_run modules/apps/foundation/portal-search/portal-search-test com.liferay.portal.search.facet.faceted.searcher.test.FacetedSearcherTest
+test_run modules/apps/foundation/portal-search/portal-search-test com.liferay.portal.search.facet.faceted.searcher.test.ModifiedFacetTest
+test_run modules/apps/foundation/portal-search/portal-search-test com.liferay.portal.search.facet.faceted.searcher.test.ScopeFacetTest
+test_run modules/apps/foundation/portal-search/portal-search-test com.liferay.portal.search.indexer.test.IndexerPostProcessorRegistryTest
+test_run modules/apps/foundation/portal-search/portal-search-test com.liferay.portal.search.internal.test.SearchPermissionCheckerTest
+
+test_run modules/apps/collaboration/bookmarks/bookmarks-test com.liferay.bookmarks.search.test.BookmarksEntrySearchTest 
+test_run modules/apps/collaboration/bookmarks/bookmarks-test com.liferay.bookmarks.search.test.BookmarksFolderSearchTest
+
+test_run modules/apps/forms-and-workflow/calendar/calendar-test com.liferay.calendar.search.test.CalendarSearcherTest
+
+test_run modules/apps/collaboration/document-library/document-library-test com.liferay.document.library.search.test.DLFileEntrySearchTest
+
+test_run modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-test com.liferay.dynamic.data.lists.search.test.DDLRecordSearchTest
+
+test_run modules/apps/web-experience/journal/journal-test com.liferay.journal.asset.test.JournalArticleAssetSearchTest
+test_run modules/apps/web-experience/journal/journal-test com.liferay.journal.search.test.JournalArticleIndexableTest
+test_run modules/apps/web-experience/journal/journal-test com.liferay.journal.service.test.JournalArticleIndexVersionsTest
+test_run modules/apps/web-experience/journal/journal-test com.liferay.journal.search.test.JournalArticleSearchTest
+test_run modules/apps/web-experience/journal/journal-test com.liferay.journal.search.test.JournalFolderSearchTest
+test_run modules/apps/web-experience/journal/journal-test com.liferay.journal.search.test.JournalIndexerTest
+
+test_run modules/apps/collaboration/wiki/wiki-test com.liferay.wiki.search.test.WikiPageSearchTest
+}
+
+function run_some_tests()
+{
+	test_run modules/apps/foundation/portal-search/portal-search-test com.liferay.portal.search.journal.search.test.JournalArticleIndexerLocalizedContentTest
+}
 
 function ant_deploy()
 {
@@ -24,7 +87,7 @@ function gradle_deploy()
 	${LIFERAY_PORTAL_DIR}/gradlew deploy
 }
 
-function test_prepare()
+function test_run()
 {
 	subdir=$1
 	tests=$2
@@ -32,30 +95,15 @@ function test_prepare()
 	figlet -f mini test prepare $1 || true
 
 	cd ${LIFERAY_PORTAL_DIR}/$subdir
+
 	${LIFERAY_PORTAL_DIR}/gradlew testIntegration --tests $tests --stacktrace || { 
 		RETURN_CODE=$?
 		echo ${RETURN_CODE}
 		echo "*** IGNORING BOGUS FAILURE & MOVING ON! :-)"
 	}
+
+	open build/reports/tests/testIntegration/index.html
 }
 
 
-ant_deploy portal-kernel
-ant_deploy portal-test
-ant_deploy portal-test-integration
-
-gradle_deploy modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-test-util
-
-test_prepare modules/apps/foundation/portal-search/portal-search-test com.liferay.portal.search.facet.faceted.searcher.test.FacetedSearcherTest
-test_prepare modules/apps/collaboration/bookmarks/bookmarks-test com.liferay.bookmarks.search.test.BookmarksEntrySearchTest 
-test_prepare modules/apps/collaboration/bookmarks/bookmarks-test com.liferay.bookmarks.search.test.BookmarksFolderSearchTest
-test_prepare modules/apps/collaboration/document-library/document-library-test com.liferay.document.library.search.test.DLFileEntrySearchTest
-test_prepare modules/apps/collaboration/wiki/wiki-test com.liferay.wiki.search.test.WikiPageSearchTest
-test_prepare modules/apps/forms-and-workflow/calendar/calendar-test com.liferay.calendar.search.test.CalendarSearcherTest
-test_prepare modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-test com.liferay.dynamic.data.lists.search.test.DDLRecordSearchTest
-test_prepare modules/apps/web-experience/journal/journal-test com.liferay.journal.asset.test.JournalArticleAssetSearchTest
-test_prepare modules/apps/web-experience/journal/journal-test com.liferay.journal.search.test.JournalArticleIndexableTest
-test_prepare modules/apps/web-experience/journal/journal-test com.liferay.journal.service.test.JournalArticleIndexVersionsTest
-test_prepare modules/apps/web-experience/journal/journal-test com.liferay.journal.search.test.JournalArticleSearchTest
-test_prepare modules/apps/web-experience/journal/journal-test com.liferay.journal.search.test.JournalFolderSearchTest
-test_prepare modules/apps/web-experience/journal/journal-test com.liferay.journal.search.test.JournalIndexerTest
+main
