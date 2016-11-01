362065ddbaead606d4cb181ef2715457e4c90d2a
André de Oliveira <andre.oliveira@liferay.com>
2016-10-31 17:53:31 -0700

    Run Journal tests from its very own split repo if need be

--- sh/arquillian-search-tests-run-all.sh
+++ sh/arquillian-search-tests-run-all.sh
@@ -2,8 +2,9 @@
 
 set -o errexit ; set -o nounset
 
-RUN_ALL_TESTS=false
+RUN_ALL_TESTS=true
 REDEPLOY_ALL_DEPENDENCIES=false
+JOURNAL_IN_SPLITREPO=false
 
 
 function main() 
@@ -36,35 +37,51 @@ gradle_deploy modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-
 #
 function run_all_tests()
 {
-test_run modules/apps/foundation/portal-search/portal-search-test com.liferay.portal.search.facet.faceted.searcher.test.AssetTagNamesFacetTest
-test_run modules/apps/foundation/portal-search/portal-search-test com.liferay.portal.search.facet.faceted.searcher.test.FacetedSearcherTest
-test_run modules/apps/foundation/portal-search/portal-search-test com.liferay.portal.search.facet.faceted.searcher.test.ModifiedFacetTest
-test_run modules/apps/foundation/portal-search/portal-search-test com.liferay.portal.search.facet.faceted.searcher.test.ScopeFacetTest
-test_run modules/apps/foundation/portal-search/portal-search-test com.liferay.portal.search.indexer.test.IndexerPostProcessorRegistryTest
-test_run modules/apps/foundation/portal-search/portal-search-test com.liferay.portal.search.internal.test.SearchPermissionCheckerTest
+test_run foundation/portal-search/portal-search-test com.liferay.portal.search.facet.faceted.searcher.test.AssetTagNamesFacetTest
+test_run foundation/portal-search/portal-search-test com.liferay.portal.search.facet.faceted.searcher.test.FacetedSearcherTest
+test_run foundation/portal-search/portal-search-test com.liferay.portal.search.facet.faceted.searcher.test.ModifiedFacetTest
+test_run foundation/portal-search/portal-search-test com.liferay.portal.search.facet.faceted.searcher.test.ScopeFacetTest
+test_run foundation/portal-search/portal-search-test com.liferay.portal.search.indexer.test.IndexerPostProcessorRegistryTest
+test_run foundation/portal-search/portal-search-test com.liferay.portal.search.internal.test.SearchPermissionCheckerTest
 
-test_run modules/apps/collaboration/bookmarks/bookmarks-test com.liferay.bookmarks.search.test.BookmarksEntrySearchTest 
-test_run modules/apps/collaboration/bookmarks/bookmarks-test com.liferay.bookmarks.search.test.BookmarksFolderSearchTest
+test_run collaboration/bookmarks/bookmarks-test com.liferay.bookmarks.search.test.BookmarksEntrySearchTest 
+test_run collaboration/bookmarks/bookmarks-test com.liferay.bookmarks.search.test.BookmarksFolderSearchTest
 
-test_run modules/apps/forms-and-workflow/calendar/calendar-test com.liferay.calendar.search.test.CalendarSearcherTest
+test_run forms-and-workflow/calendar/calendar-test com.liferay.calendar.search.test.CalendarSearcherTest
 
-test_run modules/apps/collaboration/document-library/document-library-test com.liferay.document.library.search.test.DLFileEntrySearchTest
+test_run collaboration/document-library/document-library-test com.liferay.document.library.search.test.DLFileEntrySearchTest
 
-test_run modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-test com.liferay.dynamic.data.lists.search.test.DDLRecordSearchTest
+test_run forms-and-workflow/dynamic-data-lists/dynamic-data-lists-test com.liferay.dynamic.data.lists.search.test.DDLRecordSearchTest
 
-test_run modules/apps/web-experience/journal/journal-test com.liferay.journal.asset.test.JournalArticleAssetSearchTest
-test_run modules/apps/web-experience/journal/journal-test com.liferay.journal.search.test.JournalArticleIndexableTest
-test_run modules/apps/web-experience/journal/journal-test com.liferay.journal.service.test.JournalArticleIndexVersionsTest
-test_run modules/apps/web-experience/journal/journal-test com.liferay.journal.search.test.JournalArticleSearchTest
-test_run modules/apps/web-experience/journal/journal-test com.liferay.journal.search.test.JournalFolderSearchTest
-test_run modules/apps/web-experience/journal/journal-test com.liferay.journal.search.test.JournalIndexerTest
+if [ "$JOURNAL_IN_SPLITREPO" = true ]
+then
 
-test_run modules/apps/collaboration/wiki/wiki-test com.liferay.wiki.search.test.WikiPageSearchTest
+test_run_splitrepo com-liferay-journal/journal-test com.liferay.journal.asset.test.JournalArticleAssetSearchTest
+test_run_splitrepo com-liferay-journal/journal-test com.liferay.journal.search.test.JournalArticleIndexableTest
+test_run_splitrepo com-liferay-journal/journal-test com.liferay.journal.service.test.JournalArticleIndexVersionsTest
+test_run_splitrepo com-liferay-journal/journal-test com.liferay.journal.search.test.JournalArticleSearchTest
+test_run_splitrepo com-liferay-journal/journal-test com.liferay.journal.search.test.JournalFolderSearchTest
+test_run_splitrepo com-liferay-journal/journal-test com.liferay.journal.search.test.JournalIndexerTest
+
+else
+
+test_run web-experience/journal/journal-test com.liferay.journal.asset.test.JournalArticleAssetSearchTest
+test_run web-experience/journal/journal-test com.liferay.journal.search.test.JournalArticleIndexableTest
+test_run web-experience/journal/journal-test com.liferay.journal.service.test.JournalArticleIndexVersionsTest
+test_run web-experience/journal/journal-test com.liferay.journal.search.test.JournalArticleSearchTest
+test_run web-experience/journal/journal-test com.liferay.journal.search.test.JournalFolderSearchTest
+test_run web-experience/journal/journal-test com.liferay.journal.search.test.JournalIndexerTest
+
+fi
+
+test_run collaboration/wiki/wiki-test com.liferay.wiki.search.test.WikiPageSearchTest
 }
 
 function run_some_tests()
 {
-	test_run modules/apps/foundation/portal-search/portal-search-test com.liferay.portal.search.journal.search.test.JournalArticleIndexerLocalizedContentTest
+test_run_splitrepo com-liferay-journal/journal-test com.liferay.journal.search.test.JournalArticleIndexerLocalizedContentTest
+
+test_run web-experience/journal/journal-test com.liferay.journal.asset.test.JournalArticleAssetSearchTest.testOrderByTitle*
 }
 
 function ant_deploy()
@@ -87,17 +104,21 @@ function gradle_deploy()
 	${LIFERAY_PORTAL_DIR}/gradlew deploy
 }
 
-function test_run()
+function do_test_run()
 {
-	subdir=$1
+	directory=$1
 	tests=$2
+	no_settings_gradle=$3
 
-	figlet -f digital RUN $1 || true
+	figlet -f digital RUN $directory || true
 
-	cd ${LIFERAY_PORTAL_DIR}/$subdir
+	cd $directory
 
-	mv settings.gradle settings.gradle.ORIGINAL || true
-	mv ../settings.gradle ../settings.gradle.ORIGINAL || true
+	if [ "$no_settings_gradle" = true ]
+	then
+		mv settings.gradle settings.gradle.ORIGINAL || true
+		mv ../settings.gradle ../settings.gradle.ORIGINAL || true
+	fi
 
 	${LIFERAY_PORTAL_DIR}/gradlew testIntegration --tests $tests --stacktrace || { 
 		RETURN_CODE=$?
@@ -105,10 +126,35 @@ function test_run()
 		echo "*** IGNORING BOGUS FAILURE & MOVING ON! :-)"
 	}
 
-	mv settings.gradle.ORIGINAL settings.gradle || true
-	mv ../settings.gradle.ORIGINAL ../settings.gradle || true	
+	if [ "$no_settings_gradle" = true ]
+	then
+		mv settings.gradle.ORIGINAL settings.gradle || true
+		mv ../settings.gradle.ORIGINAL ../settings.gradle || true	
+	fi
 
 	open build/reports/tests/testIntegration/index.html || true
+}
+
+function test_run()
+{
+	subdir=$1
+	tests=$2
+
+	directory=${LIFERAY_PORTAL_DIR}/modules/apps/$subdir
+	no_settings_gradle=true
+
+	do_test_run $directory $tests $no_settings_gradle
+}
+
+function test_run_splitrepo()
+{
+	splitrepo=$1
+	tests=$2
+
+	directory=${LIFERAY_PORTAL_DIR}/../$splitrepo
+	no_settings_gradle=false
+
+	do_test_run $directory $tests $no_settings_gradle
 }
 
 
